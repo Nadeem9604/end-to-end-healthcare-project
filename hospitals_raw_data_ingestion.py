13f535cbbfeb03a89e5074ce6225813cc12a6d2e
Nadeem Shaikh <156994704+Nadeem9604@users.noreply.github.com>
2026-05-30 07:49:14 -0500

    Add files via upload

---
+++ hospitals_raw_data_ingestion.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+
+source_path = "abfss://data@healthcareproject88.dfs.core.windows.net/staging/hospitals/"
+checkpoint_path = "abfss://data@healthcareproject88.dfs.core.windows.net/bronze/hospitals_raw/checkpoint/"
+schema_location = "abfss://data@healthcareproject88.dfs.core.windows.net/bronze/hospitals_raw/schema/"
+
+# Autoloader read
+df = (spark.readStream
+          .format("cloudFiles")
+          .option("cloudFiles.format", "csv")
+          .option("header", "true")
+          .option("inferSchema", "true")
+          .option("cloudFiles.maxFilesPerTrigger", 1) # READ ONE FILE AT A TIME
+          .option("cloudFiles.schemaLocation", schema_location)  
+          .load(source_path)
+     )
+
+# Write Bronze table (append)
+(
+    df.drop("_rescued_data")
+      .writeStream
+      .format("delta")
+      .option("checkpointLocation", checkpoint_path)
+      .outputMode("append")
+      .trigger(availableNow=True)   # AVAILABLE NOW → ingests all files & stops
+      .toTable("healthcareextcatlog.bronze.hospitals_raw")
+)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select * from healthcareextcatlog.bronze.hospitals_raw
